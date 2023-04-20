22cc7e8720d724ccdb1aaeef625556dc5fb8358b
mhusam792 <106589725+mhusam792@users.noreply.github.com>
2023-04-20 18:25:03 +0200

    Update main.py

--- 11_guess_the_number/main.py
+++ 11_guess_the_number/main.py
@@ -1,6 +1,9 @@
 import random
 from art import logo
 
+# generate a random number
+rand_num = random.randint(1, 100)
+
 print(logo)
 print("Welcome to the Number Guessing Game!")
 print("I'm thinking of a number between 1 and 100.")
@@ -8,7 +11,7 @@ print("I'm thinking of a number between 1 and 100.")
 choices = ['hard', 'easy']
 difficulty = input("Choose a difficulty. Type'easy' or 'hard': ").strip().lower()
 
-# handling the input of difficulty it it was not 'hard' or 'easy'
+# handling the input of difficulty if it was not 'hard' or 'easy'
 while difficulty not in choices:
         print("Please enter difficulty level bettwen 'hard' or 'easy'.\n")
         difficulty = input("Choose a difficulty. Type'easy' or 'hard': ").strip().lower()
@@ -18,30 +21,35 @@ if difficulty == 'easy':
 elif difficulty == 'hard':
     num_attempts = 5
 
-# generate a random number
-rand_num = random.randint(1, 100)
-
-while num_attempts > 0:
-    print(f"You have {num_attempts} attempts remaining to guess the number.")
-
-    # handling the guessing number if it was a string
+# handling the guessing number if it was a string
+def checking_is_number():
     num_type = True
     while num_type:
         try:
+            global guessing
             guessing = int(input("Make a guess: "))
             num_type = False
         except:
             print("please inter a number not strings\n")
             continue
-    
-    if rand_num == guessing:
-        print(f"You got it! The answer was {rand_num}")
+
+def checking_guess(rand_number, user_guessing):
+    if rand_number == user_guessing:
+        print(f"You got it! The answer was {rand_number}")
         print("Guess a gain.")
-        break
-    elif guessing > rand_num:
+    elif user_guessing > rand_number:
         print("Too high.")
     else:
         print("Too low.")
+
+while num_attempts > 0:
+    print(f"You have {num_attempts} attempts remaining to guess the number.")
+
+    checking_is_number()
+    checking_guess(rand_num, guessing)
+
+    if rand_num == guessing:
+        break
 
     num_attempts -= 1
     if num_attempts == 0:
